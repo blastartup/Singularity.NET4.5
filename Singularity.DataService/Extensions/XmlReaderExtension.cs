1fcd2d6740e6d87fe6ad0ce6561d7551be8a650b
Dean Goddard <dean@funkeysolutions.com.au>
2018-04-18 14:32:55 +1000

    Add IsEndElement() extension to XmlReader.

---
+++ Singularity.DataService/Extensions/XmlReaderExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Singularity.DataService.Extensions
+{
+	public static class XmlReaderExtension
+	{
+		public static Boolean IsEndElement(this XmlReader xmlReader, String name = "")
+		{
+			if (xmlReader.NodeType == XmlNodeType.EndElement)
+			{
+				if (name == String.Empty)
+				{
+					return true;
+				}
+
+				return (xmlReader.Name == name);
+			}
+
+			return false;
+		}
+	}
+}
